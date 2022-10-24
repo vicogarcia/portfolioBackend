ec012bf071f29950f6ac6a18a0545cdf82b1aecf
Victoria Garcia <garcia.victoria88@gmail.com>
2022-10-24 19:55:17 -0300

    cambios de rutas para trabajar en desarrollo o produccion

--- src/main/java/com/portfolioweb/gv/controller/EducacionController.java
+++ src/main/java/com/portfolioweb/gv/controller/EducacionController.java
@@ -30,6 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/educacion")
+//@CrossOrigin(origins = "http://localhost:4200")
 @CrossOrigin(origins = "https://portfolioftd.web.app")
 public class EducacionController {
     @Autowired
